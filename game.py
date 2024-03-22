f4ced56654ea20fd9afe8987100c66c19e11a910
Tomas Lucentini <tomlucen@gmail.com>
2024-03-22 12:29:30 -0300

    Modificacion 7C

--- game.py
+++ game.py
@@ -3,6 +3,20 @@ import random
 # Lista de palabras posibles
 words = ["python", "programación", "computadora", "código", "desarrollo", "inteligencia"]
 
+# Función para obtener la palabra parcialmente adivinada según el nivel de dificultad
+def get_partial_word(secret_word, level, guessed_letters):
+    if level == "facil":
+        # Mostrar todas las vocales por defecto
+        return "".join([letter if letter in guessed_letters or letter in "aeiou" else "_" for letter in secret_word])
+    elif level == "medio":
+        # Mostrar la primera y la última letra de la palabra
+        revealed_letters = set([secret_word[0], secret_word[-1]]) & set(guessed_letters)
+        #return "".join([letter if letter in guessed_letters or letter in revealed_letters else "_" for letter in secret_word])
+        return "".join([letter if letter in guessed_letters or letter in revealed_letters or index == 0 or index == len(secret_word)-1 else "_" for index, letter in enumerate(secret_word)])
+    else:
+        # Mostrar todas las letras adivinadas hasta el momento
+        return "".join([letter if letter in guessed_letters else "_" for letter in secret_word])
+
 # Elegir una palabra al azar
 secret_word = random.choice(words)
 # Número máximo de fallos permitidos
@@ -11,10 +25,17 @@ max_failures = 5
 guessed_letters = []
 
 print("¡Bienvenido al juego de adivinanzas!")
-print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
+print("Selecciona el nivel de dificultad: fácil, medio o difícil")
+level = input("Nivel: ").lower()
 
-word_displayed = "_" * len(secret_word)
-# Mostrarla palabra parcialmente adivinada
+# Verificar si el nivel seleccionado es válido
+if level not in ["facil", "medio", "dificil"]:
+    print("Nivel de dificultad no válido. Seleccionando nivel fácil por defecto.")
+    level = "facil"
+
+print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
+# Mostrar la palabra parcialmente adivinada según el nivel seleccionado
+word_displayed = get_partial_word(secret_word, level, guessed_letters)
 print(f"Palabra: {word_displayed}")
 
 failures = 0  # Contador de fallos
@@ -43,14 +64,9 @@ while failures < max_failures:
         failures += 1  # Incrementar el contador de fallos
    
     # Mostrar la palabra parcialmente adivinada
-    letters = []
-    for letter in secret_word:
-        if letter in guessed_letters:
-            letters.append(letter)
-        else:
-            letters.append("_")
-    word_displayed = "".join(letters)
+    word_displayed = get_partial_word(secret_word, level, guessed_letters)
     print(f"Palabra: {word_displayed}")
+    
     # Verificar si se ha adivinado la palabra completa
     if word_displayed == secret_word:
         print(f"¡Felicidades! Has adivinado la palabra secreta: {secret_word}")
